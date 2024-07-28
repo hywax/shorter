f4b9aa1e810d1ddfefe7149b0cd83502168f9ccf
hywax <me@hywax.space>
2024-07-28 14:51:19 +0500

    chore: tailwind config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,3 +1,17 @@
-import config from '@shorter/theme-config/tailwind.config'
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
-export default config
+export default <Partial<Config>>{
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: 'rgb(var(--ui-background) / <alpha-value>)',
+        foreground: 'rgb(var(--ui-foreground) / <alpha-value>)',
+      },
+    },
+  },
+}
