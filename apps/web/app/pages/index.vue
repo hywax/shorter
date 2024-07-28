b1267158124024f9f53b43269a483ec18d37c98b
hywax <me@hywax.space>
2024-07-28 15:00:06 +0500

    chore: prepare index page

--- apps/web/app/pages/index.vue
+++ apps/web/app/pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Home page</h1>
+  <UPageHeader :title="$t('projects.title')" />
+
+  <UContainer>
+    <!-- <PageEmpty
+      icon="i-heroicons:cube-transparent-20-solid"
+      :title="$t('projects.greeting.title')"
+      :description="$t('projects.greeting.description')"
+    >
+      <template #action>
+        <UButton
+          icon="i-heroicons:plus"
+          :label="$t('projects.create.action')"
+        />
+      </template>
+    </PageEmpty> -->
+  </UContainer>
 </template>
+
+<script setup lang="ts">
+</script>
