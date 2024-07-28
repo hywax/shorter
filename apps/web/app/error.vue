d9d94c0ffd22ee5b4ce6e79c61a2d1fd9d706fe6
hywax <me@hywax.space>
2024-07-28 14:57:57 +0500

    chore: create error page

---
+++ apps/web/app/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLayout name="empty">
+    <UPageError :error="error" class="min-h-screen" />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{
+  error: NuxtError
+}>()
+</script>
