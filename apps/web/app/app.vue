41953b4639070a175da477617217ad12464d1daf
hywax <me@hywax.space>
2024-07-28 14:49:05 +0500

    chore: app meta tags

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -1,7 +1,26 @@
 <template>
   <NuxtRouteAnnouncer />
-  <NuxtLoadingIndicator color="var(--p-primary-color)" />
+  <NuxtLoadingIndicator color="rgb(var(--color-primary-500))" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const { meta } = useAppConfig()
+const i18nHead = useLocaleHead({
+  addSeoAttributes: {},
+  addDirAttribute: true,
+  identifierAttribute: 'id',
+})
+
+useHead({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs?.lang,
+    dir: i18nHead.value.htmlAttrs?.dir,
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])],
+  titleTemplate: (productCategory) => productCategory ? `${productCategory} - ${meta.name}` : meta.name,
+})
+</script>
