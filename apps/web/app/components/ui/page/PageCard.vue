28ae719b8ee6bef4318028f337ae2d1590dcab79
hywax <me@hywax.space>
2024-08-05 16:21:27 +0500

    chore: create page components

---
+++ apps/web/app/components/ui/page/PageCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <UCard>
+    <div v-if="title || description" class="flex flex-col space-y-2 mb-6">
+      <h2 v-if="title" class="text-xl font-medium">
+        {{ title }}
+      </h2>
+      <p v-if="description" class="text-sm text-gray-500">
+        {{ description }}
+      </p>
+    </div>
+
+    <slot />
+
+    <template v-if="$slots['footer-left'] || $slots['footer-right']" #footer>
+      <div class="flex items-center justify-between">
+        <div>
+          <slot name="footer-left" />
+        </div>
+        <div>
+          <slot name="footer-right" />
+        </div>
+      </div>
+    </template>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title?: string
+  description?: string
+}
+
+defineProps<Props>()
+</script>
---
+++ apps/web/app/components/ui/page/PageToolbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-[49px] flex-shrink-0 flex items-center border-b border-gray-200 dark:border-gray-800 px-4 py-2 gap-x-4">
+    <div class="flex items-center justify-between flex-1 gap-x-1.5">
+      <slot>
+        <div class="flex items-stretch gap-1.5">
+          <slot name="left" />
+        </div>
+
+        <div class="flex items-stretch gap-1.5">
+          <slot name="right" />
+        </div>
+      </slot>
+    </div>
+  </div>
+</template>
